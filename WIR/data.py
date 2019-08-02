5a3532eaec12bef71debd245a0cde64d1cd418ea
Davide <loiudice.1691799@studenti.uniroma1.it>
2019-08-02 16:40:29 +0200

    aggiunto train2.csv che è uguale a train.csv ma il delimitatore delle colonne cambia in ; da ,

--- WIR/data.py
+++ WIR/data.py
@@ -1,7 +1,7 @@
 import os
 import shutil
 import sys  ########
-PATH="C:\\Users\\Salvatore\\Desktop\\WIR\\"  #da modificare per il vostro path
+PATH="/home/biar/Desktop/WIR/progettoGIT/WIR"  #da modificare per il vostro path
 
 
 non_bmp_map = dict.fromkeys(range(0x10000, sys.maxunicode + 1), 0xfffd)
