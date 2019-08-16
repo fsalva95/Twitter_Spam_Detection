1678c1e39631cece6728cb18f069cc79dd77af6c
Davide <davy.bankai@gmail.com>
2019-08-16 13:31:21 +0200

    aggiunto chiSquareRank2 che usa solo sklearn e produce grafici

--- WIR/chiSquareRank2.py
+++ WIR/chiSquareRank2.py
@@ -1,84 +1,23 @@
 import pandas as pd
 import numpy as np
-import scipy.stats as stats
-from scipy.stats import chi2_contingency
-from decimal import Decimal
 
 toBeSorted= []
 toBeSortedCHI= []
 
-class ChiSquare:
-    def __init__(self, dataframe):
-        self.df = dataframe
-        self.p = None #P-Value
-        self.chi2 = None #Chi Test Statistic
-        self.dof = None
-        
-        self.dfObserved = None
-        self.dfExpected = None
-        
-    def _print_chisquare_result(self, colX, alpha):
-        result = ""
-        if self.p<alpha:
-            result="{0} is IMPORTANT for Prediction".format(colX)
-        else:
-            result="{0} is NOT an important predictor. (Discard {0} from model)".format(colX)
-
-        #tmp=Decimal(self.p)
-        #tmp2=round(tmp,25)
-        #print("p-value of {0}".format(colX))
-        #print(self.p)
-        #print("chi2 of {0}".format(colX))
-        #print(self.chi2)
-        #print(result)
-        #print("degree of freedom:")
-        #print(self.dof)
-        toBeSorted.append((colX, self.p))
-        toBeSortedCHI.append((colX, self.chi2))
-        
-    def TestIndependence(self,colX,colY, alpha=0.05):
-        X = self.df[colX].astype(str)
-        Y = self.df[colY].astype(str)
-        
-        self.dfObserved = pd.crosstab(Y,X) 
-        chi2, p, dof, expected = stats.chi2_contingency(self.dfObserved.values)
-        self.p = p
-        self.chi2 = chi2
-        self.dof = dof 
-        
-        self.dfExpected = pd.DataFrame(expected, columns=self.dfObserved.columns, index = self.dfObserved.index)
-        
-        self._print_chisquare_result(colX,alpha)
-
 df = pd.pandas.read_csv("trainchisquare.csv", delimiter=";")
 df['dummyCat'] = np.random.choice([0, 1], size=(len(df),), p=[0.5, 0.5])#QUESTA RIGA POI SI RIMUOVE, SERVE SOLO A CREARE UNA COLONNA CON UN VALORE RANDOMICO PER DIMOSTRARE CHE TYPE E QUESTA COLONNA SONO INDIPENDENTI
 
-#Initialize ChiSquare Class
-cT = ChiSquare(df)
+
 
 #Feature Selection
 testColumns = ['following','followers','actions', 'is_retweet', 'URLCounted', 'HashtagCounted', 'MensionCounted', 'averageHashtag', 'averageURL', 'wordsCounted', 'SpamWordsCounted', 'dummyCat']
 #actions;is_retweet;URLCounted;;$;;HashtagCounted;;$;;MensionCounted
-test20=['actions']
-for var in testColumns:
-    cT.TestIndependence(colX=var,colY="Type" )  
-toBeSorted.sort(key = lambda x: x[1])
-print("\n")
-print(toBeSorted)
-print("\n")
-#print("NOW THE CHI2")
-#print(toBeSortedCHI)
-toBeSortedCHI.sort(key = lambda x: x[1])
-toBeSortedCHI.reverse()
-print("\n")
-print("THE RANK OF ATTRIBUTES IS (chi2):")
-print(toBeSortedCHI)
-print("\n")
-
 
 data = df    
 #X = data.drop(['Type', 'following'], axis='columns')  #independent columns
 #y = data.Type    #target column i.e price range
+
+#sta cosa è perchè talvolta mi dava un errore tipo cannot convert float to string
 from sklearn import preprocessing
 le = preprocessing.LabelEncoder()
 data = data.apply(le.fit_transform)
