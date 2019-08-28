816b8b8207c6cd071fc107172a51091670e86ff5
Davide <davy.bankai@gmail.com>
2019-08-28 11:13:44 +0200

    graph

--- WIR/main.py
+++ WIR/main.py
@@ -7,8 +7,8 @@ from sklearn.svm import SVC
 from sklearn.metrics import classification_report, confusion_matrix
 import numpy as np
 import matplotlib.pyplot as plt
-
-
+from matplotlib.colors import ListedColormap
+import warnings
 
 
 filename = "data_train.txt"
@@ -29,7 +29,7 @@ with open(filename, 'r',encoding="utf8") as file:
 
         i=0
         for row in file:
-                if i!=0 and len(row.split(";;$;;"))==12 and i<100:# i<500 per vedere che il codice funziona (è da togliere per trainare tutto il db
+                if i!=0 and len(row.split(";;$;;"))==12 and i<10:# i<500 per vedere che il codice funziona (è da togliere per trainare tutto il db
                         value=row.split(";;$;;")
                         classification=value[4]
                         value=list(map(str.strip,value))
@@ -38,7 +38,9 @@ with open(filename, 'r',encoding="utf8") as file:
                         value.remove(value[4])  #rimuovo classificazione
                         value=list(map(float,value))
                         value=np.asarray(value)
+                        np.set_printoptions(suppress=True)
                         #print(value.shape)
+                        print(value)
                         X.append(value)
                         #print(value)
                         #print(row)
@@ -50,9 +52,9 @@ print("FATTO")
 #print(Y)
 X=np.asarray(X)
 train_x, test_x, train_y, test_y = train_test_split(X, Y, test_size = 0.20)
-svclassifier = SVC(kernel='rbf', gamma='auto')
+svclassifier = SVC(kernel='rbf', random_state=0, gamma=.01, C=1)
 print(train_x.shape)
-model=svclassifier.fit(train_x, train_y)
+svclassifier.fit(train_x, train_y)
 print("FATTO")
 y_pred = svclassifier.predict(test_x)
 print("FATTO")
@@ -61,7 +63,7 @@ print(confusion_matrix(test_y,y_pred))
 print(classification_report(test_y,y_pred))
 
 
-def make_meshgrid(x, y, h=.02):
+'''def make_meshgrid(x, y, h=.02):
     """Create a mesh of points to plot in
 
     Parameters
@@ -119,6 +121,81 @@ ax.set_xticks(())
 ax.set_yticks(())
 ax.set_title(title)
 
+plt.show()'''
+
+def versiontuple(v):
+    return tuple(map(int, (v.split("."))))
+
+
+def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02):
+
+    # setup marker generator and color map
+    markers = ('s', 'x', 'o', '^', 'v')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+
+    # plot the decision surface
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                           np.arange(x2_min, x2_max, resolution))
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    Z = Z.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, Z, alpha=0.4, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y == cl, 0], y=X[y == cl, 1],
+                    alpha=0.8, c=cmap(idx),
+                    marker=markers[idx], label=cl)
+
+    # highlight test samples
+    if test_idx:
+        # plot all samples
+        if not versiontuple(np.__version__) >= versiontuple('1.9.0'):
+            X_test, y_test = X[list(test_idx), :], y[list(test_idx)]
+            warnings.warn('Please update to NumPy 1.9.0 or newer')
+        else:
+            X_test, y_test = X[test_idx, :], y[test_idx]
+
+        plt.scatter(X_test[:, 0],
+                    X_test[:, 1],
+                    c='',
+                    alpha=1.0,
+                    linewidths=1,
+                    marker='o',
+                    s=55, label='test set')
+
+df0=[]; #QUALITY
+df1=[]; #SPAM
+for i in range(0,len(X)) :
+    if Y[i]=='Quality':
+        df0.append(X[i])
+    else :
+        df1.append(X[i])
+df0=np.asarray(df0)
+df1=np.asarray(df1)
+#df0=df0.astype(int)
+#df1=df1.astype(int)
+plt.scatter(df0[:,0],
+            df0[:,1],
+            c='b', marker='x',
+            label='quality')
+plt.scatter(df1[:,0],
+            df1[:,1],
+            c='r',
+            marker='s',
+            label='spam')
+
+plt.xlim([-3, 3])
+plt.ylim([-3, 3])
+plt.legend(loc='best')
+plt.tight_layout()
+# Visualize the decision boundaries
+plot_decision_regions(X, Y, classifier=svclassifier)
+plt.legend(loc='upper left')
+plt.tight_layout()
 plt.show()
 
 
--- WIR/proveGraph.py
+++ WIR/proveGraph.py
@@ -1,5 +1,12 @@
 import pandas as pd
 import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from sklearn import svm
+from sklearn.metrics import classification_report, confusion_matrix
+from matplotlib.colors import ListedColormap
+import warnings
+import matplotlib.pyplot as plt
 
 toBeSorted= []
 toBeSortedCHI= []
@@ -53,9 +60,19 @@ X.columns[selector2.get_support(indices=True)]
 vector_names2=list(X.columns[selector2.get_support(indices=True)])
 #print(vector_names2)
 
+train_x, test_x, train_y, test_y = train_test_split(X, y, test_size = 0.20)
+'''svclassifier = SVC(kernel='rbf', random_state=0, gamma=.01, C=1)
+print(train_x.shape)
+svclassifier.fit(train_x, train_y)
+print("FATTO")
+y_pred = svclassifier.predict(test_x)
+print("FATTO")
 
-
-z
+print(confusion_matrix(test_y,y_pred))
+print(classification_report(test_y,y_pred))
+print(X)
+'''
+'''
 
 import matplotlib.pyplot as plt
 
@@ -86,5 +103,72 @@ plt.figure(num=None, figsize=(10, 6), dpi=80, facecolor='w', edgecolor='k')
 plt.bar(ns_df_sorted2.Feat_names, ns_df_sorted2.Scores, color='r', align='center')
 #plt.bar(vector_names2, selector2.scores_[indices2[range(10)]], color='r', align='center')
 plt.show()
+'''
+def make_meshgrid(x, y, h=.02):
+    """Create a mesh of points to plot in
+
+    Parameters
+    ----------
+    x: data to base x-axis meshgrid on
+    y: data to base y-axis meshgrid on
+    h: stepsize for meshgrid, optional
+
+    Returns
+    -------
+    xx, yy : ndarray
+    """
+    x_min, x_max = x.min() - 1, x.max() + 1
+    y_min, y_max = y.min() - 1, y.max() + 1
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
+                         np.arange(y_min, y_max, h))
+    return xx, yy
 
 
+def plot_contours(ax, clf, xx, yy, **params):
+    """Plot the decision boundaries for a classifier.
+
+    Parameters
+    ----------
+    ax: matplotlib axes object
+    clf: a classifier
+    xx: meshgrid ndarray
+    yy: meshgrid ndarray
+    params: dictionary of params to pass to contourf, optional
+    """
+    Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+    out = ax.contourf(xx, yy, Z, **params)
+    return out
+C = 1.0  # SVM regularization parameter
+models = (svm.SVC(kernel='linear', C=C),
+          svm.LinearSVC(C=C),
+          svm.SVC(kernel='rbf', gamma=0.7, C=C),
+          svm.SVC(kernel='poly', degree=3, C=C))
+models = (clf.fit(X, y) for clf in models)
+
+# title for the plots
+titles = ('SVC with linear kernel',
+          'LinearSVC (linear kernel)',
+          'SVC with RBF kernel',
+          'SVC with polynomial (degree 3) kernel')
+
+# Set-up 2x2 grid for plotting.
+fig, sub = plt.subplots(2, 2)
+plt.subplots_adjust(wspace=0.4, hspace=0.4)
+
+X0, X1 = X.iloc[0:2, 0], X.iloc[0:2, 1]
+xx, yy = make_meshgrid(X0, X1)
+
+for clf, title, ax in zip(models, titles, sub.flatten()):
+    plot_contours(ax, clf, xx, yy,
+                  cmap=plt.cm.coolwarm, alpha=0.8)
+    ax.scatter(X0, X1, c=y, cmap=plt.cm.coolwarm, s=20, edgecolors='k')
+    ax.set_xlim(xx.min(), xx.max())
+    ax.set_ylim(yy.min(), yy.max())
+    ax.set_xlabel('Sepal length')
+    ax.set_ylabel('Sepal width')
+    ax.set_xticks(())
+    ax.set_yticks(())
+    ax.set_title(title)
+
+plt.show()
