1112ba9b105a35c9911600fe1f575569c2f04dbb
Davide <davy.bankai@gmail.com>
2019-08-28 09:48:16 +0200

    push

--- WIR/main.py
+++ WIR/main.py
@@ -6,6 +6,9 @@ import shutil
 from sklearn.svm import SVC
 from sklearn.metrics import classification_report, confusion_matrix
 import numpy as np
+import matplotlib.pyplot as plt
+
+
 
 
 filename = "data_train.txt"
@@ -26,7 +29,7 @@ with open(filename, 'r',encoding="utf8") as file:
 
         i=0
         for row in file:
-                if i!=0 and len(row.split(";;$;;"))==12 and i<500:# i<500 per vedere che il codice funziona (è da togliere per trainare tutto il db
+                if i!=0 and len(row.split(";;$;;"))==12 and i<100:# i<500 per vedere che il codice funziona (è da togliere per trainare tutto il db
                         value=row.split(";;$;;")
                         classification=value[4]
                         value=list(map(str.strip,value))
@@ -47,15 +50,86 @@ print("FATTO")
 #print(Y)
 X=np.asarray(X)
 train_x, test_x, train_y, test_y = train_test_split(X, Y, test_size = 0.20)
-svclassifier = SVC(kernel='linear')
+svclassifier = SVC(kernel='rbf', gamma='auto')
 print(train_x.shape)
-svclassifier.fit(train_x, train_y)
+model=svclassifier.fit(train_x, train_y)
 print("FATTO")
 y_pred = svclassifier.predict(test_x)
 print("FATTO")
 
 print(confusion_matrix(test_y,y_pred))
 print(classification_report(test_y,y_pred))
+
+
+def make_meshgrid(x, y, h=.02):
+    """Create a mesh of points to plot in
+
+    Parameters
+    ----------
+    x: data to base x-axis meshgrid on
+    y: data to base y-axis meshgrid on
+    h: stepsize for meshgrid, optional
+
+    Returns
+    -------
+    xx, yy : ndarray
+    """
+    x_min, x_max = x.min() - 1, x.max() + 1
+    y_min, y_max = y.min() - 1, y.max() + 1
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
+                         np.arange(y_min, y_max, h))
+    return xx, yy
+
+def plot_contours(ax, clf, xx, yy, **params):
+    """Plot the decision boundaries for a classifier.
+
+    Parameters
+    ----------
+    ax: matplotlib axes object
+    clf: a classifier
+    xx: meshgrid ndarray
+    yy: meshgrid ndarray
+    params: dictionary of params to pass to contourf, optional
+    """
+    Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+    out = ax.contourf(xx, yy, Z, **params)
+    return out
+
+
+
+titles = ('SVC with RBF')
+
+# Set-up 2x2 grid for plotting.
+fig, sub = plt.subplots(2, 2)
+plt.subplots_adjust(wspace=0.4, hspace=0.4)
+
+X0, X1 = X[:, 0], X[:, 1]
+xx, yy = make_meshgrid(X0, X1)
+
+
+plot_contours(ax, clf, xx, yy,
+              cmap=plt.cm.coolwarm, alpha=0.8)
+ax.scatter(X0, X1, c=Y, cmap=plt.cm.coolwarm, s=20, edgecolors='k')
+ax.set_xlim(xx.min(), xx.max())
+ax.set_ylim(yy.min(), yy.max())
+ax.set_xlabel('Sepal length')
+ax.set_ylabel('Sepal width')
+ax.set_xticks(())
+ax.set_yticks(())
+ax.set_title(title)
+
+plt.show()
+
+
+
+
+
+
+
+
+
+
 
 
 
