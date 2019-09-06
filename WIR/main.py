fc5802f630ff350dfd66994191062f3d8f51ae1a
fsalva95 <52610638+fsalva95@users.noreply.github.com>
2019-09-06 16:27:07 +0200

    Add files via upload

--- WIR/main.py
+++ WIR/main.py
@@ -5,10 +5,12 @@ from sklearn.model_selection import train_test_split
 import shutil
 from sklearn.svm import SVC
 from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.preprocessing import MinMaxScaler
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
 import warnings
+#from mlxtend.plotting import plot_decision_regions
 
 
 filename = "data_train.txt"
@@ -29,8 +31,9 @@ with open(filename, 'r',encoding="utf8") as file:
 
         i=0
         for row in file:
-                if i!=0 and len(row.split(";;$;;"))==12 and i<10:# i<500 per vedere che il codice funziona (è da togliere per trainare tutto il db
+                if i!=0 and len(row.split(";;$;;"))==12:
                         value=row.split(";;$;;")
+                        
                         classification=value[4]
                         value=list(map(str.strip,value))
                         Y.append(classification)
@@ -38,165 +41,35 @@ with open(filename, 'r',encoding="utf8") as file:
                         value.remove(value[4])  #rimuovo classificazione
                         value=list(map(float,value))
                         value=np.asarray(value)
-                        np.set_printoptions(suppress=True)
-                        #print(value.shape)
-                        print(value)
+
+                        
                         X.append(value)
-                        #print(value)
-                        #print(row)
+
                 i=i+1
 
 
-#print(X) #troppo pesante
-print("FATTO")
-#print(Y)
 X=np.asarray(X)
-train_x, test_x, train_y, test_y = train_test_split(X, Y, test_size = 0.20)
-svclassifier = SVC(kernel='rbf', random_state=0, gamma=.01, C=1)
+y=[]
+for i in range(0,len(X)) :
+    if Y[i]=='Quality':
+        y.append(0.0)
+    else :
+        y.append(1.0)
+
+
+scaler= MinMaxScaler((0,1))
+X= scaler.fit_transform(X)
+train_x, test_x, train_y, test_y = train_test_split(X, y, test_size = 0.20)
+svclassifier = SVC(kernel='rbf',gamma=1000,C=1)
 print(train_x.shape)
 svclassifier.fit(train_x, train_y)
-print("FATTO")
+
 y_pred = svclassifier.predict(test_x)
-print("FATTO")
+
 
 print(confusion_matrix(test_y,y_pred))
 print(classification_report(test_y,y_pred))
 
-
-'''def make_meshgrid(x, y, h=.02):
-    """Create a mesh of points to plot in
-
-    Parameters
-    ----------
-    x: data to base x-axis meshgrid on
-    y: data to base y-axis meshgrid on
-    h: stepsize for meshgrid, optional
-
-    Returns
-    -------
-    xx, yy : ndarray
-    """
-    x_min, x_max = x.min() - 1, x.max() + 1
-    y_min, y_max = y.min() - 1, y.max() + 1
-    xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
-                         np.arange(y_min, y_max, h))
-    return xx, yy
-
-def plot_contours(ax, clf, xx, yy, **params):
-    """Plot the decision boundaries for a classifier.
-
-    Parameters
-    ----------
-    ax: matplotlib axes object
-    clf: a classifier
-    xx: meshgrid ndarray
-    yy: meshgrid ndarray
-    params: dictionary of params to pass to contourf, optional
-    """
-    Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
-    Z = Z.reshape(xx.shape)
-    out = ax.contourf(xx, yy, Z, **params)
-    return out
-
-
-
-titles = ('SVC with RBF')
-
-# Set-up 2x2 grid for plotting.
-fig, sub = plt.subplots(2, 2)
-plt.subplots_adjust(wspace=0.4, hspace=0.4)
-
-X0, X1 = X[:, 0], X[:, 1]
-xx, yy = make_meshgrid(X0, X1)
-
-
-plot_contours(ax, clf, xx, yy,
-              cmap=plt.cm.coolwarm, alpha=0.8)
-ax.scatter(X0, X1, c=Y, cmap=plt.cm.coolwarm, s=20, edgecolors='k')
-ax.set_xlim(xx.min(), xx.max())
-ax.set_ylim(yy.min(), yy.max())
-ax.set_xlabel('Sepal length')
-ax.set_ylabel('Sepal width')
-ax.set_xticks(())
-ax.set_yticks(())
-ax.set_title(title)
-
-plt.show()'''
-
-def versiontuple(v):
-    return tuple(map(int, (v.split("."))))
-
-
-def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02):
-
-    # setup marker generator and color map
-    markers = ('s', 'x', 'o', '^', 'v')
-    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
-    cmap = ListedColormap(colors[:len(np.unique(y))])
-
-    # plot the decision surface
-    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
-    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
-    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
-                           np.arange(x2_min, x2_max, resolution))
-    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
-    Z = Z.reshape(xx1.shape)
-    plt.contourf(xx1, xx2, Z, alpha=0.4, cmap=cmap)
-    plt.xlim(xx1.min(), xx1.max())
-    plt.ylim(xx2.min(), xx2.max())
-
-    for idx, cl in enumerate(np.unique(y)):
-        plt.scatter(x=X[y == cl, 0], y=X[y == cl, 1],
-                    alpha=0.8, c=cmap(idx),
-                    marker=markers[idx], label=cl)
-
-    # highlight test samples
-    if test_idx:
-        # plot all samples
-        if not versiontuple(np.__version__) >= versiontuple('1.9.0'):
-            X_test, y_test = X[list(test_idx), :], y[list(test_idx)]
-            warnings.warn('Please update to NumPy 1.9.0 or newer')
-        else:
-            X_test, y_test = X[test_idx, :], y[test_idx]
-
-        plt.scatter(X_test[:, 0],
-                    X_test[:, 1],
-                    c='',
-                    alpha=1.0,
-                    linewidths=1,
-                    marker='o',
-                    s=55, label='test set')
-
-df0=[]; #QUALITY
-df1=[]; #SPAM
-for i in range(0,len(X)) :
-    if Y[i]=='Quality':
-        df0.append(X[i])
-    else :
-        df1.append(X[i])
-df0=np.asarray(df0)
-df1=np.asarray(df1)
-#df0=df0.astype(int)
-#df1=df1.astype(int)
-plt.scatter(df0[:,0],
-            df0[:,1],
-            c='b', marker='x',
-            label='quality')
-plt.scatter(df1[:,0],
-            df1[:,1],
-            c='r',
-            marker='s',
-            label='spam')
-
-plt.xlim([-3, 3])
-plt.ylim([-3, 3])
-plt.legend(loc='best')
-plt.tight_layout()
-# Visualize the decision boundaries
-plot_decision_regions(X, Y, classifier=svclassifier)
-plt.legend(loc='upper left')
-plt.tight_layout()
-plt.show()
 
 
 
