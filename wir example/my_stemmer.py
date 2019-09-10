77098cd1f5ed7754022d213c203f10409022ce7d
Davide <davy.bankai@gmail.com>
2019-09-10 17:58:25 +0200

    removed wirExample and train2

--- wir example/my_stemmer.py
+++
@@ -1,9 +0,0 @@
-from nltk.stem.porter import *
-
-plurals = ['caresses', 'flies', 'dies', 'mules', 'denied','died', 'agreed', 'owned', 'humbled', 'sized','meeting', 'stating', 'siezing', 'itemization','sensational', 'traditional', 'reference', 'colonizer','plotted']
-
-stemmer = PorterStemmer()
-
-singles = [stemmer.stem(plural) for plural in plurals]
-
-print(' '.join(singles))
--- wir example/my_tokenize.py
+++
@@ -1,7 +0,0 @@
-from nltk.tokenize import word_tokenize, sent_tokenize
- 
-s = "Good muffins cost $3.88\nin New York.  Please buy me two of them.\n\nThanks."
-
-print(word_tokenize(s))
-
-print(sent_tokenize(s))
